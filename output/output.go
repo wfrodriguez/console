57a37e04c6d25d332945ae2200b76f39fd5f38bf
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-12-04 09:58:49 -0500

    :recycle: (refac) Cambio en ajuste de texto
    En la función `PrintSection` dependiendo del ancho de la consola se desordenaba el texto a mostrar, se realiza una nueva
    
    modificación para que se visualice de forma correcta teniendo en cuenta el ancho de la consola.

--- output/output.go
+++ output/output.go
@@ -7,11 +7,12 @@ import (
 	"unicode/utf8"
 
 	"github.com/wfrodriguez/console"
+	"github.com/wfrodriguez/console/util"
 )
 
 var reColor *regexp.Regexp
 
-func init() {
+func init() {  // {{{
 	reColor = regexp.MustCompile(`(<[^>]+>)`)
 
 	reset := NewStyle(WithForeground(creset))
@@ -26,11 +27,11 @@ func init() {
 	AddStyle("info", NewStyle(WithForeground(New256Color(39, Foreground))))
 	AddStyle("warning", NewStyle(WithForeground(New256Color(220, Foreground))))
 	AddStyle("error", NewStyle(WithForeground(New256Color(196, Foreground))))
-}
+}  // }}}
 
 // Sprintf Formatea y retorna un string de igual forma que `fmt.Sprintf`, pero con colores a traves de etiquetas de
 // colores encerrados en `<` y `>` y finalizando con la etiqueta `<reset>` o `</>`
-func Sprintf(format string, a ...any) string {
+func Sprintf(format string, a ...any) string {  // {{{
 	txt := format
 	out := reColor.ReplaceAllStringFunc(
 		txt,
@@ -47,60 +48,68 @@ func Sprintf(format string, a ...any) string {
 	)
 
 	return fmt.Sprintf(out, a...)
-}
+}  // }}}
 
 // PrintSection Imprime una sección de texto en la consola
 // con una línea de guiones
-func PrintSection(styleName, title string, content ...string) {
+func PrintSection(styleName, title string, content ...string) {  // {{{
 	width, _ := console.GetConsoleSize()
 	sb := strings.Builder{}
+	wl := width - 3
+	ln := len(content)
 	line := strings.Repeat("─", width-(utf8.RuneCountInString(title)+3))
 	sb.WriteString(fmt.Sprintf("<%s>┌┤%s├%s\n", styleName, title, line))
-	for _, row := range content {
-		sb.WriteString(fmt.Sprintf("│%s\n", row))
+	for i, row := range content {
+		wrap := strings.Split(util.WordWrap(row, wl), "\n")
+		for _, r := range wrap {
+			sb.WriteString(fmt.Sprintf("│%s\n", r))
+		}
+		if i < ln-1 {
+			sb.WriteString("│\n")
+		}
 	}
 	sb.WriteString("└───</>")
 	fmt.Println(Sprintf(sb.String()))
-}
+}  // }}}
 
 // Title crea una cabecera estilo Setext de primer nivel
-func Title(title string) {
+func Title(title string) {  // {{{
 	l := utf8.RuneCountInString(title)
 	fmt.Println(Sprintf("<title>%s\n%s</>", title, strings.Repeat("=", l)))
-}
+}  // }}}
 
 // Subtitle crea una cabecera estilo Setext de segundo nivel
-func Subtitle(title string) {
+func Subtitle(title string) {  // {{{
 	l := utf8.RuneCountInString(title)
 	fmt.Println(Sprintf("<subtitle>%s\n%s</>", title, strings.Repeat("-", l)))
-}
+}  // }}}
 
 // Note crea un bloque de texto de tipo nota
-func Note(content ...string) {
-	PrintSection("note", "Nota", content...)
-}
+func Note(content ...string) {  // {{{
+	PrintSection("note", "NOTE", content...)
+}  // }}}
 
 // Caution crea un bloque de texto de tipo advertencia
-func Caution(content ...string) {
+func Caution(content ...string) {  // {{{
 	PrintSection("caution", "CAUTION!", content...)
-}
+}  // }}}
 
 // Success crea un bloque de texto de tipo éxito
-func Success(content ...string) {
+func Success(content ...string) {  // {{{
 	PrintSection("success", "SUCCESS", content...)
-}
+}  // }}}
 
 // Info crea un bloque de texto de tipo información
-func Info(content ...string) {
-	PrintSection("info", "Información", content...)
-}
+func Info(content ...string) {  // {{{
+	PrintSection("info", "INFO", content...)
+}  // }}}
 
 // Warning crea un bloque de texto de tipo advertencia
-func Warning(content ...string) {
+func Warning(content ...string) {  // {{{
 	PrintSection("warning", "WARNING!!", content...)
-}
+}  // }}}
 
 // Error crea un bloque de texto de tipo error
-func Error(content ...string) {
+func Error(content ...string) {  // {{{
 	PrintSection("error", "ERROR!!!", content...)
-}
+}  // }}}
